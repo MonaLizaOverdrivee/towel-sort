d3f7c131de188797f0c74ba8b591a4bb13de9675
MonaLizaOverdrivee <santospele@mail.ru>
2021-07-09 16:53:16 +0300

    create logic

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix.map((itm, i) => (i + 1) % 2  ? itm : itm.reverse()).flat()
 }
